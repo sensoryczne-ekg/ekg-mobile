1bc05936b0b1c36b3131957041fdd9d6833375bd
Maciej Pawłowski <maciekpawlowski1@gmail.com>
2024-03-02 19:49:12 +0100

    Add fetching records

--- app/src/main/java/com/pawlowski/ekgmonitor/ui/screens/chart/ChartViewModel.kt
+++ app/src/main/java/com/pawlowski/ekgmonitor/ui/screens/chart/ChartViewModel.kt
@@ -1,16 +1,43 @@
 package com.pawlowski.ekgmonitor.ui.screens.chart
 
+import androidx.lifecycle.viewModelScope
 import com.pawlowski.ekgmonitor.BaseMviViewModel
 import com.pawlowski.ekgmonitor.domain.Resource
+import com.pawlowski.ekgmonitor.domain.getDataOrNull
+import com.pawlowski.ekgmonitor.domain.useCase.StreamRecords
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import javax.inject.Inject
 
 @HiltViewModel
 internal class ChartViewModel
     @Inject
-    constructor() : BaseMviViewModel<ChartState, ChartEvent>(
+    constructor(
+        private val streamRecords: StreamRecords,
+    ) : BaseMviViewModel<ChartState, ChartEvent>(
             initialState = ChartState(recordsResource = Resource.Loading),
         ) {
+        override fun initialised() {
+            streamRecords()
+                .onEach { newRecord ->
+                    updateState {
+                        copy(
+                            recordsResource =
+                                Resource.Success(
+                                    data = (recordsResource.getDataOrNull() ?: listOf()) + newRecord,
+                                ),
+                        )
+                    }
+                }.catch {
+                    it.printStackTrace()
+                    updateState {
+                        copy(recordsResource = Resource.Error(it))
+                    }
+                }.launchIn(viewModelScope)
+        }
+
         override fun onNewEvent(event: ChartEvent) {
         }
     }
