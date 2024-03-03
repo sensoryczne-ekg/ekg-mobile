0576f310b55f9bf1f769413bd3f3baa58e132f97
Maciej Pawłowski <maciekpawlowski1@gmail.com>
2024-03-03 21:21:27 +0100

    delete tls

--- libs/network/src/main/java/com/pawlowski/network/channel/GetGrpcChannelUseCase.kt
+++ libs/network/src/main/java/com/pawlowski/network/channel/GetGrpcChannelUseCase.kt
@@ -2,12 +2,9 @@ package com.pawlowski.network.channel
 
 import android.app.Application
 import io.grpc.Channel
-import io.grpc.okhttp.OkHttpChannelBuilder
+import io.grpc.android.AndroidChannelBuilder
 import javax.inject.Inject
 import javax.inject.Singleton
-import javax.net.ssl.SSLContext
-import javax.net.ssl.TrustManager
-import javax.net.ssl.X509TrustManager
 
 @Singleton
 internal class GetGrpcChannelUseCase
@@ -16,37 +13,8 @@ internal class GetGrpcChannelUseCase
         private val context: Application,
     ) : IGetGrpcChannelUseCase {
         override operator fun invoke(): Channel =
-            OkHttpChannelBuilder
+            AndroidChannelBuilder
                 .forAddress("srv3.enteam.pl", 6001)
-                .sslSocketFactory(noTrustedContext().socketFactory)
-                .hostnameVerifier { _, _ -> true }
+                .usePlaintext()
                 .build()
-
-        private fun noTrustedContext(): SSLContext {
-            val trustAllCerts =
-                arrayOf<TrustManager>(
-                    object : X509TrustManager {
-                        override fun getAcceptedIssuers(): Array<java.security.cert.X509Certificate> {
-                            return arrayOf()
-                        }
-
-                        override fun checkClientTrusted(
-                            certs: Array<java.security.cert.X509Certificate>,
-                            authType: String,
-                        ) {
-                        }
-
-                        override fun checkServerTrusted(
-                            certs: Array<java.security.cert.X509Certificate>,
-                            authType: String,
-                        ) {
-                        }
-                    },
-                )
-
-            val sslContext = SSLContext.getInstance("SSL")
-            sslContext.init(null, trustAllCerts, java.security.SecureRandom())
-
-            return sslContext
-        }
     }
